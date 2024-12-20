e8d1af1f46851f0622f6b6f203d59249ad4e5f30
Sam MC <Salem Nouhou>
2024-12-20 14:37:12 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Canvas } from "@react-three/fiber"
 
 function App() {
   return (
-    <Canvas>
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshPhongMaterial />
-      </mesh>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[0, 0, 5]} color="red" />
-    </Canvas>
+    // <Canvas>
+    //   <mesh>
+    //     <boxGeometry args={[2, 2, 2]} />
+    //     <meshPhongMaterial />
+    //   </mesh>
+    //   <ambientLight intensity={0.1} />
+    //   <directionalLight position={[0, 0, 5]} color="red" />
+    // </Canvas>
+    <>
+   <span className="text-red-500">
+    dd
+   </span>
+    </>
   )
 }
 
